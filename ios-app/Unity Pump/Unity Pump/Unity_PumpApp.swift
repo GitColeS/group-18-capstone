6b920911cf0c54712f6fe8cc73575fe8a0d8d289
Emma Starkman <emmastarkman@icloud.com>
2026-02-06 16:00:22 -0500

    Add Unity Pump iOS SwiftUI project scaffold

---
+++ ios-app/Unity Pump/Unity Pump/Unity_PumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unity_PumpApp.swift
+//  Unity Pump
+//
+//  Created by Emma Starkman on 2026-02-06.
+//
+
+import SwiftUI
+
+@main
+struct Unity_PumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
